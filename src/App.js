ca0c2646e4587a77d333b61956f76c9b664eaac3
harshi2309cmd <narapureddyharshitha@gmail.com>
2023-12-28 02:19:54 -0600

    Added Html 	modified:   public/index.html 	modified:   src/App.js 	new file:   src/Weather.css 	new file:   src/Weather.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Weather from './Weather';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-     Weather App
+     <Weather />
+     <footer>
+      This Project is coded by {""}
+      <a href="" >Harshitha Reddy</a>{""}and is{""}  
+     <a href="https://github.com/harshi2309cmd/react-weather-app">Open-sourced at Github</a>
+     </footer>
     </div>
   );
 }
